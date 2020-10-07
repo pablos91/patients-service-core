c24be2c69a84ba02fd04843be0bf7a41a7a73674
Paweł Niewiadomski <pafciu@yahoo.pl>
2020-10-07 23:57:26 +0200

    deleted attributes from db model

--- PatientsService/Models/Patient.cs
+++ PatientsService/Models/Patient.cs
@@ -10,17 +10,10 @@ namespace PatientsService.Models
     public class Patient
     {
         public int Id { get; set; }
-        [Required]
-        [StringLength(50)]
         public string FirstName { get; set; }
-        [Required]
-        [StringLength(100)]
         public string LastName { get; set; }
-        [Required]
         public DateTime BirthDate { get; set; }
-        [Required]
         public DateTime TestDate { get; set; }
-        [Required]
         public bool TestPositive { get; set; }
         public DateTime SysDate { get; set; }
     }
