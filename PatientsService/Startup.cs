41282d9f6303088b68f03fd18b01b909139db09e
Paweł Niewiadomski <pafciu@yahoo.pl>
2020-11-23 19:36:07 +0100

    added dockerfile added swagger for bearer token

--- PatientsService/Startup.cs
+++ PatientsService/Startup.cs
@@ -45,6 +45,30 @@ namespace PatientsService
                     Version = "v1",
                     Description = string.Empty
                 });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
             });
             services.AddScoped<ServiceBusSender>();
 
